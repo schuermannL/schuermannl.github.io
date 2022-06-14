e47dd18d9f92f37b4ecb50efc67df589b0f0b882
schuermannL <l.schuermann@posteo.de>
2022-06-14 13:51:51 +0200

    update new pic

---
+++ important_commands.R
@@ -0,0 +1,19 @@
+library(blogdown)
+
+# most important fucntions
+
+# 1. Creating the site
+new_site(theme = "wowchemy/starter-academic")
+
+# 2. Serve site
+blogdown::serve_site()
+
+# 3. Create a blogpost
+blogdown::new_post("My new post")
+
+# 4. Build site (fills in the public)
+blogdown::build_site()
+
+
+
+build_site()
